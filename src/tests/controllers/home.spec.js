f2be184dcfb986674bced081ac4473bc09e65aa8
Mike Henderyckx <mike.henderyckx@gmail.com>
2016-11-24 00:31:21 +0100

    FE-tests for home.controller.js

--- src/tests/controllers/home.spec.js
+++ src/tests/controllers/home.spec.js
@@ -3,24 +3,12 @@
 
     describe('ngFlux.HomeController', function () {
 
-        var $controller,
-            $injector,
-            $scope;
-
-        var HomeStore,
-            HomeActioncreator;
-
-        var src;
-
-        function $getController() {
-            $scope = $rootScope.$new();
-            src = $controller('apps.eidlogin.LoginController', {
-                '$scope': $scope,
-                'ngFlux.HomeActioncreator': $injector.get('ngFlux.HomeActioncreator'),
-                'ngFlux.HomeStore': $injector.get('ngFlux.HomeStore')
-            });
-            return src;
-        }
+        var $injector,
+            $controller,
+            $scope,
+            $rootScope,
+            $state,
+            $timeout;
 
         beforeEach(module('ngFlux'));
 
@@ -30,25 +18,93 @@
             })
         );
 
-        beforeEach(
-            module(function ($provide) {
-                $provide.store('ngFlux.HomeStore', function () {
-                    return HomeStore;
-                });
-                $provide.factory('nngFlux.HomeActioncreator', function () {
-                    return HomeActioncreator;
-                });
-            })
-        );
+        beforeEach(function suitArrange() {
+            $controller = $injector.get('$controller');
+            $rootScope = $injector.get('$rootScope');
+            $state = $injector.get('$state');
+            $timeout = $injector.get('$timeout');
+        });
 
+        var src;
+
+        function $getController() {
+            $scope = $rootScope.$new();
+
+            src = $controller('ngFlux.HomeController', {
+                '$scope': $scope,
+                'ngFlux.HomeActioncreator': $injector.get('ngFlux.HomeActioncreator'),
+                'ngFlux.HomeStore': $injector.get('ngFlux.HomeStore')
+            });
+            return src;
+        }
 
         it('should be registered', function () {
             expect(function () {
-                // $injector.get('ngFlux.HomeController');
                 $getController();
             }).to.not.throw(Error);
         });
 
+        describe('$scope.$listenTo', function () {
+
+            it('should listen to the HomeStore \'setUsername\' event', function () {
+                var store = $injector.get('ngFlux.HomeStore');
+
+                sinon.spy($rootScope, '$listenTo');
+
+                $getController();
+                expect($rootScope.$listenTo.calledWith(store, ['username'])).to.eql(true);
+
+                $rootScope.$listenTo.restore();
+            });
+
+            it('should listen to the HomeStore \'setUser\' event', function () {
+                var store = $injector.get('ngFlux.HomeStore');
+
+                sinon.spy($rootScope, '$listenTo');
+
+                $getController();
+                expect($rootScope.$listenTo.calledWith(store, ['user'])).to.eql(true);
+
+                $rootScope.$listenTo.restore();
+            });
+
+        });
+
+        describe('Trigger actions', function () {
+
+            it('should run setUsername() action', function () {
+                var actioncreator = $injector.get('ngFlux.HomeActioncreator');
+                sinon.spy(actioncreator, 'setUsername');
+
+                $getController().setUsername('Mikey');
+
+                expect(actioncreator.setUsername.calledWith('Mikey')).to.equal(true);
+            });
+
+            it('should run setUser() action', function () {
+                var userObj = {
+                    firstName: 'Mike',
+                    lastName: 'Henderyckx',
+                    age: 0
+                };
+                var actioncreator = $injector.get('ngFlux.HomeActioncreator');
+                sinon.spy(actioncreator, 'setUser');
+
+                $getController().setUser(userObj);
+
+                expect(actioncreator.setUser.calledWith(userObj)).to.equal(true);
+            });
+
+            it('should run getUsername() action', function () {
+                var actioncreator = $injector.get('ngFlux.HomeActioncreator');
+                sinon.spy(actioncreator, 'getUsername');
+
+                $getController().getUsername();
+
+                expect(actioncreator.getUsername.called).to.equal(true);
+            });
+
+        });
 
     });
 
